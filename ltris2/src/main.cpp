096ce8cba2f350f5d22775c19a7af86d82b418c1
kulkanie <kulkanie@gmx.net>
2024-06-08 12:46:08 +0200

    added makefile stuff

---
+++ ltris2/src/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * main.cpp
+ * (C) 2024 by Michael Speck
+ */
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
